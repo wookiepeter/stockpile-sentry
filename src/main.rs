b4f05cc5f46490261c08d3e4d970092a1bba3580
Matthis Hagen <matthis.hagen@o2online.de>
2025-06-16 23:55:47 +0200

    Update html response

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn handle_read(mut stream: &TcpStream) {
 }
 
 fn handle_write(mut stream: TcpStream) {
-    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Deployed Container is reachable from web!</body></html>\r\n";
     match stream.write_all(response) {
         Ok(_) => println!("Response sent"),
         Err(e) => println!("Failed sending response: {}", e),
